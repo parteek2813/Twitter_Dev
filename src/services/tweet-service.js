351dfa9d7049df9c5d03c8f1e13eaf180413087f
parteek kumar <digitalparteek@gmail.com>
2023-07-04 14:59:42 +0530

    Created comment on comment

--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,4 +1,4 @@
-import {TweetRepository,HashtagRepository } from "../repository/index.js"
+import { TweetRepository, HashtagRepository } from "../repository/index.js";
 
 class TweetService {
   constructor() {
@@ -8,23 +8,29 @@ class TweetService {
 
   async create(data) {
     const content = data.content;
-    const tags = content.match(/#[a-zA-Z0-9_]+/g)
-                .map((tag) => tag.substring(1).toLowerCase())
-    
+    const tags = content
+      .match(/#[a-zA-Z0-9_]+/g)
+      .map((tag) => tag.substring(1).toLowerCase());
+
     const tweet = await this.tweetRepository.create(data);
 
     let alreadyPresenttags = await this.hashtagRepository.findByName(tags);
-    let titleOfPresentTags = alreadyPresenttags.map(tags => tags.title)
+    let titleOfPresentTags = alreadyPresenttags.map((tags) => tags.title);
     let newTags = tags.filter((tag) => !titleOfPresentTags.includes(tag));
     newTags = newTags.map((tag) => {
       return { title: tag, tweets: [tweet.id] };
     });
 
     await this.hashtagRepository.bulkCreate(newTags);
-    alreadyPresenttags.forEach((tag) =>{
+    alreadyPresenttags.forEach((tag) => {
       tag.tweets.push(tweet.id);
       tag.save();
-    })
+    });
+    return tweet;
+  }
+
+  async get(tweetId) {
+    const tweet = await this.tweetRepository.getWithComments(tweetId);
     return tweet;
   }
 }
