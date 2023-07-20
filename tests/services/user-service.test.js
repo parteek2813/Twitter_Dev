dcde3deae227d2c24f2849191bc06a573fbce783
parteek kumar <digitalparteek@gmail.com>
2023-07-20 23:34:54 +0530

    added userService test

---
+++ tests/services/user-service.test.js
@@ -0,0 +1,22 @@
+import UserService from "../../src/services/user-services.js";
+import UserRepository from "../../src/repository/user-repository.js";
+
+jest.mock("../../src/repository/user-repository.js");
+
+describe("user service signup test", () => {
+  test("Should successfully create a user", async () => {
+    const data = {
+      email: "a@b.com",
+      password: "123456",
+    };
+
+    UserRepository.prototype.create.mockReturnValue({
+      ...data,
+      createdAt: "2023-01-06",
+      updatedAt: "2023-02-09",
+    });
+    const service = new UserService();
+    const response = await service.signup();
+    expect(response.email).toBe(data.email);
+  });
+});
