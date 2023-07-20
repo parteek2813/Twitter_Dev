68146a01f39ba969237b4c6eaf420b80faeaad5e
parteek kumar <digitalparteek@gmail.com>
2023-07-20 19:14:09 +0530

    additional testcase

--- tests/repository/tweet-repository.test.js
+++ tests/repository/tweet-repository.test.js
@@ -12,7 +12,7 @@ test("Should create a new tweet and return it", async () => {
   //   whenever say tweet.create --- just mocks its implementation
   //   appending the data with more contents
   //   whenever we have to mock the implementation of any object, pass the Whole object[Tweet] and some method
-  // defined in it and then just mockImplementation
+  //   defined in it and then just mockImplementation
   const spy = jest.spyOn(Tweet, "create").mockImplementation(() => {
     return { ...data, createdAt: "2022-01-12", updatedAt: "2022-01-12" };
   });
@@ -23,3 +23,28 @@ test("Should create a new tweet and return it", async () => {
   expect(tweet.content).toEqual(data.content);
   expect(tweet.createdAt).toBeDefined();
 });
+
+test("should not create the tweet and throw exception", async () => {
+  const data = {
+    content: "Testing tweet ",
+  };
+
+  const spy = jest.spyOn(Tweet, "create").mockImplementation(() => {
+    throw new Error("Something went wrong");
+  });
+  const tweetRepository = new TweetRepository();
+  const tweet = await tweetRepository.create(data).catch((err) => {
+    expect(err).toBeInstanceOf(Error);
+    expect(err.message).toBe("Something went wrong");
+  });
+});
+
+// test("actually callign the model", async () => {
+//   const data = {
+//     content:
+//       "dfksnjskajnaskdjnsakdnjdfksnjskajnaskdjnsakdnjdfksnjskajnaskdjnsakdnjdfksnjskajnaskdjnsakdnjdfksnjskajnaskdjnsakdnjdfksnjskajnaskdjnsakdnjdfksnjskajnaskdjnsakdnjdfksnjskajnaskdjnsakdnjdfksnjskajnaskdjnsakdnjdfksnjskajnaskdjnsakdnjdfksnjskajnaskdjnsakdnjdfksnjskajnaskdjnsakdnjdfksnjskajnaskdjnsakdnjdfksnjskajnaskdjnsakdnjdfksnjskajnaskdjnsakdnj",
+//   };
+
+//   const tweet = await Tweet.create(data);
+//   expect(tweet).toBeUndefined();
+// });
